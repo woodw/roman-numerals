c8c88931391eb854a2ea389ff9cb26abd58814f0
bill wood <billwood.dev@gmail.com>
2016-09-07 22:56:01 -0400

    refactor: change switch cases to Object Key list

--- js/roman-numeral/roman-numeral.js
+++ js/roman-numeral/roman-numeral.js
@@ -2,6 +2,21 @@ Function.RegisterNamespace("kata");
 
 kata.RomanNumeral = function () {
 	var value = '';
+	var numeral_enum = {
+		'1000':'M',
+		'900':'CM',
+		'500':'D',
+		'400':'CD',
+		'100':'C',
+		'90':'XC',
+		'50':'L',
+		'40':'XL',
+		'10':'X',
+		'9':'IX',
+		'5':'V',
+		'4':'IV',
+		'1':'I'
+	};
 
 	this.set = function (newNumeral){
 		value = newNumeral;
@@ -13,60 +28,19 @@ kata.RomanNumeral = function () {
 
 	this.setFromNumber = function (newNumber){
 		var remainder = newNumber;
+		var numeralKeys = Object.keys(numeral_enum);
+		//Keys auto sort to lowest value
+		var pointer = numeralKeys.length;
+
+		//Subtract and record numeral_enum values
 		while(remainder>0){
-			switch(true){
-				case (remainder>=1000): 
-					value += 'M';
-					remainder -= 1000;
-					break;
-				case (remainder>=900): 
-					value += 'CM';
-					remainder -= 900;
-					break;
-				case (remainder>=500):
-					value += 'D';
-					remainder -= 500;
-					break;
-				case (remainder>=400):
-					value += 'CD';
-					remainder -= 400;
-					break;
-				case (remainder>=100):
-					value += 'C';
-					remainder -= 100;
-					break;
-				case (remainder>=90):
-					value += 'XC';
-					remainder -= 90;
-					break;	
-				case (remainder>=50):
-					value += 'L';
-					remainder -= 50;
-					break;		
-				case (remainder>=40):
-					value += 'XL';
-					remainder -= 40;
-					break;	
-				case (remainder>=10):
-					value += 'X';
-					remainder -= 10;
-					break;
-				case (remainder>=9):
-					value += 'IX';
-					remainder -= 9;
-					break;
-				case (remainder>=5):
-					value += 'V';
-					remainder -= 5;
-					break;
-				case (remainder>=4):
-					value += 'IV';
-					remainder -= 4;
-					break;
-				case (remainder>=1):
-					value += 'I';
-					remainder -= 1;
-					break;
+			var keyInt = parseInt(numeralKeys[pointer]);
+			if(remainder>=keyInt){
+				remainder -= keyInt;
+				value += numeral_enum[numeralKeys[pointer]];
+			}
+			else{
+				pointer--;
 			}
 		}
 	};
